f14731d5261f8305cc15a9215b0e1c28a3f0ac58
Daan Middelkoop <daantje676@gmail.com>
2019-10-21 19:32:33 +0200

    Load monkey from file and let camera run around it

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <thread>
 #include <stdint.h>
+#include <math.h>
 
 int main() {
     std::cout << "SpaceGame v0.1\n";
@@ -16,8 +17,9 @@ int main() {
     RT::Mesh* player = rtcontext->createMesh();
 
     std::vector<float> vertices = std::vector<float>();
+    std::vector<float> uvcoords = std::vector<float>();
 
-    int something = 500;
+    int something = 20;
     for (int x = -something; x < something; x++) {
         for (int y = -something; y < something; y++) {
             vertices.push_back(0.1 * x);
@@ -31,7 +33,22 @@ int main() {
             vertices.push_back(0.1 * x + 0.05);
             vertices.push_back(0.1 * y + 0.1);
             vertices.push_back(0.0f);
+
+            float x2 = (float)x + something;
+            float y2 = (float)y + something;
+
+            float step = (1.0 / (float)(something));
+
+            uvcoords.push_back(x2 * step);
+            uvcoords.push_back(y2 * step);
+
+            uvcoords.push_back(x2 * step + step);
+            uvcoords.push_back(y2 * step);
+
+            uvcoords.push_back(x2 * step + (step / 2.0));
+            uvcoords.push_back(y2 * step + step);
         }
+
 
     }
 
@@ -50,23 +67,36 @@ int main() {
     RT::MaterialHandle material = rtcontext->createMaterial();
     material.setTexture(&texture);
 
+
+    //player->setVertices(vertices.data(), vertices.size());
+
+
     player->setMaterial(&material);
+    RT::ObjLoader objLoader = RT::ObjLoader();
+    objLoader.loadObjFile("/home/daan/projects/SpaceGame/objFiles/monkey.obj");
+    objLoader.inflate(player);
 
 
-    player->setVertices(vertices.data(), vertices.size());
-    player->build();
-    rtcontext->setCameraPosition(0.0f, 0.0f, -1.6f);
-    rtcontext->setCameraDirection(0.0f, 0.0f, 1.0f);
 
-    float camera_x = 0.0f;
+    //player->setUVCoords(uvcoords.data(), uvcoords.size());
+
+
+    //player->build();
+    rtcontext->setCameraPosition(0.0f, 0.0f, 0.0f);
+    rtcontext->setCameraDirection(0.0f, 0.0f, -1.0f);
+
+
+    float distance = 4.0f;
+    float frame = 0.0f;
 
     std::cout << "amount of pixels reserved on gpu: " << rtcontext->getPixels()->size() << "\n";
 
     float x = 0;
     while (!RT::windowShouldClose(window)) {
         //std::cout << "drawwwwww\n";
-        camera_x += 0.1f;
-        //rtcontext->setCameraPosition(0.0f, 0.0f, -2.0f - camera_x);
+        frame += 0.01f;
+        rtcontext->setCameraPosition(distance * sin(frame), 0.0, distance * cos(frame));
+        rtcontext->setCameraDirection(-distance * sin(frame), 0.0, -distance * cos(frame));
         rtcontext->draw(window);
         rtcontext->setDebugInfo(x, 0, 0);
 
